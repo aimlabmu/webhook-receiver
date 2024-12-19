6010abd1e5d833dc410f26d5ce251a1f84474892
Zaw Htet Aung <zawhtetaung.zaw@student.mahidol.ac.th>
2024-12-20 00:18:08 +0700

    fix __init__.py

--- webhook_receiver_omise/__init__.py
+++ webhook_receiver_omise/__init__.py
@@ -8,7 +8,7 @@ settings.configure(
   EMAIL_PORT=int(os.environ.get('DJANGO_EMAIL_PORT', 587)),
   EMAIL_USE_TLS=True,
   EMAIL_HOST_USER=os.environ.get('DJANGO_EMAIL_HOST_USER', ''),
-  EMAIL_HOST_PASSWORDos.environ.get('DJANGO_EMAIL_HOST_PASSWORD', ''),
+  EMAIL_HOST_PASSWORD=os.environ.get('DJANGO_EMAIL_HOST_PASSWORD', ''),
   DEFAULT_FROM_EMAIL=os.environ.get('DJANGO_EMAIL_HOST_USER', '')
 )
 
