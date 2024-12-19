c40bc0af0a10f003c8d6ef5da65961f706c4018d
Zaw Htet Aung <zawhtetaung.zaw@student.mahidol.ac.th>
2024-12-20 00:17:48 +0700

    fix __init__.py

--- webhook_receiver_omise/__init__.py
+++ webhook_receiver_omise/__init__.py
@@ -4,10 +4,12 @@ from django.conf import settings
 
 settings.configure(
   EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend',
-  EMAIL_HOST=os.env.get('DJANGO_EMAIL_HOST'),
-  EMAIL_PORT=os.env.get('DJANGO_EMAIL_PORT'),
+  EMAIL_HOST=os.environ.get('DJANGO_EMAIL_HOST', ''),
+  EMAIL_PORT=int(os.environ.get('DJANGO_EMAIL_PORT', 587)),
   EMAIL_USE_TLS=True,
-  EMAIL_HOST_USER=os.env.get('DJANGO_EMAIL_HOST_USER'),
-  EMAIL_HOST_PASSWORD=os.env.get('DJANGO_EMAIL_HOST_PASSWORD'))
+  EMAIL_HOST_USER=os.environ.get('DJANGO_EMAIL_HOST_USER', ''),
+  EMAIL_HOST_PASSWORDos.environ.get('DJANGO_EMAIL_HOST_PASSWORD', ''),
+  DEFAULT_FROM_EMAIL=os.environ.get('DJANGO_EMAIL_HOST_USER', '')
+)
 
 django.setup()
