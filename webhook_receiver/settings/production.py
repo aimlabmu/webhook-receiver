5d273f6abb675a50e43f78c5b80e14b18ced7796
Zaw Htet Aung <zawhtetaung.zaw@student.mahidol.ac.th>
2024-12-20 00:25:56 +0700

    Update production.py with email init

--- webhook_receiver/settings/production.py
+++ webhook_receiver/settings/production.py
@@ -6,7 +6,20 @@ from django.conf import settings
 
 from . import *  # noqa: F403
 
-settings.configure()
+import os
+import django
+
+settings.configure(
+  EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend',
+  EMAIL_HOST=os.environ.get('DJANGO_EMAIL_HOST', ''),
+  EMAIL_PORT=int(os.environ.get('DJANGO_EMAIL_PORT', 587)),
+  EMAIL_USE_TLS=True,
+  EMAIL_HOST_USER=os.environ.get('DJANGO_EMAIL_HOST_USER', ''),
+  EMAIL_HOST_PASSWORD=os.environ.get('DJANGO_EMAIL_HOST_PASSWORD', ''),
+  DEFAULT_FROM_EMAIL=os.environ.get('DJANGO_EMAIL_HOST_USER', '')
+)
+
+django.setup()
 
 # We inherit all settings from base.py, including the anything set in
 # the environment.  However, if the environment contains the variable
